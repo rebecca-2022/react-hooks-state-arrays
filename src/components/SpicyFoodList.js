85b6452ed58a189820d1f396af9a6249dfeff20d
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-09-24 21:46:09 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
+function handleClick(id) {
+  const newFoodArray = foods.filter((food) => food.id !== id);
+  setFoods(newFoodArray);
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
 
+}
+function handleClick(id) {
+  const newFoodArray = foods.filter((food) => food.id !== id);
+  setFoods(newFoodArray);
+}
+function handleClick(id) {
+  const newFoodArray = foods.map((food) => {
+    if (food.id === id) {
+      return {
+        ...food,
+        heatLevel: food.heatLevel + 1,
+      };
+    } else {
+      return food;
+    }
+  });
+  setFoods(newFoodArray);
+}
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={()=>handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
